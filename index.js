83d0d82375a9ea134a5c489d7c50391b9645ce87
abirahmmed12 <abirahmedchowdhury12@gmail.com>
2023-12-28 21:30:54 +0600

    new feature

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ async function run() {
     })
 
 
-    // app.post('/jwt', async (req, res) => {
+    ('/jwt', async (req, res) => {
     //   const user = req.body;
     //   console.log('user', user);
     //   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
